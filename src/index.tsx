e0c1fb6de40f5b61962a221df3f19e54b0d9bacf
Aoi <ht.so.ik0102@gmail.com>
2021-09-13 21:39:35 +0700

    change index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 //redux setup
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
@@ -12,7 +11,7 @@ import "antd/dist/antd.css";
 
 const composeEnhancers =
   (typeof window !== "undefined" &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
 const store = createStore(
@@ -28,8 +27,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
