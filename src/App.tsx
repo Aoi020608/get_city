bbd4b47c189fd3ed1836b9bf919b11fa878fe95e
Aoi <ht.so.ik0102@gmail.com>
2021-09-13 23:00:40 +0700

    create Nav.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import Number from './components/Number';
-import City from './components/City';
-
+import Nav from "./components/Nav";
+import Number from "./components/Number";
+import City from "./components/City";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Number />
       <City />
     </div>
