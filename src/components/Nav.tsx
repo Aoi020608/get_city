68708cc66b3ef9af619420980e8fa5ba51925d35
Aoi <ht.so.ik0102@gmail.com>
2021-09-13 23:09:23 +0700

    modify nav color

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,6 +12,9 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       flexGrow: 1,
     },
+    toolbar: {
+      backgroundColor: "white",
+    },
     menuButton: {
       marginRight: theme.spacing(2),
     },
@@ -27,19 +30,19 @@ const Nav: React.FC = () => {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className={classes.toolbar}>
           <IconButton
             edge="start"
             className={classes.menuButton}
-            color="inherit"
+            color="primary"
             aria-label="menu"
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            FinTech
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="primary">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
