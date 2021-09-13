ed87ffb11cb009848c2d56aecd9d1be4709c48d4
Aoi <ht.so.ik0102@gmail.com>
2021-09-13 23:27:30 +0700

    change nav baar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,6 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,7 +20,7 @@ const useStyles = makeStyles((theme: Theme) =>
       marginRight: theme.spacing(2),
     },
     title: {
-      flexGrow: 1,
+      //   flexGrow: 1,
     },
   })
 );
@@ -31,18 +32,29 @@ const Nav: React.FC = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="primary"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            FinTech
-          </Typography>
-          <Button color="primary">Login</Button>
+          <Grid container spacing={3}>
+            <Grid sm={3}>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="primary"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                FinTech
+              </Typography>
+            </Grid>
+            <Grid sm={6}>
+              <Typography variant="button" color="textSecondary">
+                PRODUCTS
+              </Typography>
+            </Grid>
+            <Grid sm={3}>
+              <Button color="primary">Login</Button>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
