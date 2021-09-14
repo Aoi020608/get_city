a5f3cb34865ac4af317e2e06745813f647143d18
Aoi <ht.so.ik0102@gmail.com>
2021-09-14 19:58:33 +0700

    change mostfeatured post

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import Nav from "./components/Nav";
 import Number from "./components/Number";
 import City from "./components/City";
+import MainFeaturedPost from "./components/MainFeatured";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <MainFeaturedPost />
       <Number />
       <City />
     </div>
