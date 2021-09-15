a3facf3f8572802800666e915356eaa7de6bc539
Aoi <ht.so.ik0102@gmail.com>
2021-09-15 08:04:10 +0700

    change the style of blog

--- src/actions/cityAction.tsx
+++ src/actions/cityAction.tsx
@@ -2,56 +2,54 @@
 import axios from "axios";
 import getCityUrl from "../api";
 
-import listCity from "../prefectureList.json";
-
 // interface Props {
 //   dispatch : Dispatch<{}>
 //  }
 
-export const fetchCity = (city_id: number) => async (dispatch: any) => {
-  //fetch axios
-  const city = await axios.get(getCityUrl(city_id));
-  console.log(city);
-  const object = city.data.data;
-  const cityObject = [];
-  for (let i = 0; i < object.length; i++) {
-    // console.log(object[i].name);
-    cityObject.push(object[i].name);
-  }
+// export const fetchCity = (city_id: number) => async (dispatch: any) => {
+//   //fetch axios
+//   const city = await axios.get(getCityUrl(city_id));
+//   console.log(city);
+//   const object = city.data.data;
+//   const cityObject = [];
+//   for (let i = 0; i < object.length; i++) {
+//     // console.log(object[i].name);
+//     cityObject.push(object[i].name);
+//   }
 
-  dispatch({
-    type: "FETCH_CITY",
-    payload: {
-      city: cityObject,
-    },
-  });
-};
+//   dispatch({
+//     type: "FETCH_CITY",
+//     payload: {
+//       city: cityObject,
+//     },
+//   });
+// };
 
-export const getAllCityName = () => (dispatch: any) => {
-  const temName = [];
-  const temId = [];
-  for (let i = 0; i < listCity.length; i++) {
-    temId.push(listCity[i].id);
-    temName.push(listCity[i].name);
-  }
-  dispatch({
-    type: "GET_ALL_CITYNAME",
-    payload: {
-      listCityId: temId,
-      listCityName: temName,
-    },
-  });
-};
+// export const getAllCityName = () => (dispatch: any) => {
+//   const temName = [];
+//   const temId = [];
+//   for (let i = 0; i < listCity.length; i++) {
+//     temId.push(listCity[i].id);
+//     temName.push(listCity[i].name);
+//   }
+//   dispatch({
+//     type: "GET_ALL_CITYNAME",
+//     payload: {
+//       listCityId: temId,
+//       listCityName: temName,
+//     },
+//   });
+// };
 
-export const selectCityName = (cityName: string) => (dispatch: any) => {
-  for (let i = 0; i < listCity.length; i++) {
-    if (listCity[i].name === cityName) {
-      dispatch({
-        type: "SELECT_CITY",
-        payload: {
-          selectCityId: listCity[i].id,
-        },
-      });
-    }
-  }
-};
+// export const selectCityName = (cityName: string) => (dispatch: any) => {
+//   for (let i = 0; i < listCity.length; i++) {
+//     if (listCity[i].name === cityName) {
+//       dispatch({
+//         type: "SELECT_CITY",
+//         payload: {
+//           selectCityId: listCity[i].id,
+//         },
+//       });
+//     }
+//   }
+// };
--- src/components/Markdown2.tsx
+++ src/components/Markdown2.tsx
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import showdown from 'showdown';
-import parse from 'html-react-parser';
-import readme from '../blog-post1.md';
+import React, { useEffect, useState } from "react";
+import { Grid, Typography } from "@mui/material/";
+import Divider from "@mui/material/Divider";
+import showdown from "showdown";
+import parse from "html-react-parser";
+import readme from "../blog-post1.md";
 
 export default function ComponentName() {
-    const [html, setHTML] = useState("");
+  const [html, setHTML] = useState("");
 
-    //Use componentDidMount(): if class based component to load md file
-    useEffect(() => {
-        fetch(readme)
-            .then(data => data.text())
-            .then(text => {
-                const converter = new showdown.Converter();
-                setHTML(converter.makeHtml(text));
-            })
-    }, []);
+  //Use componentDidMount(): if class based component to load md file
+  useEffect(() => {
+    fetch(readme)
+      .then((data) => data.text())
+      .then((text) => {
+        const converter = new showdown.Converter();
+        setHTML(converter.makeHtml(text));
+      });
+  }, []);
 
-    return (
-        <div>{parse(html)}</div>
-    )
+  return (
+    <Grid
+      item
+      style={{ marginTop: "4rem"}}
+    >
+      <Typography variant="h6" gutterBottom>
+        Title
+      </Typography>
+      <Divider />
+      <div>{parse(html)}</div>
+    </Grid>
+  );
 }
