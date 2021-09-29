d235ad74769976507f386539de0bb7aaab1e88ac
Aoi <ht.so.ik0102@gmail.com>
2021-09-29 21:16:25 +0700

    remove warning in Wirks file

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -4,7 +4,6 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
