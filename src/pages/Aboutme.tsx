c9ccbb994a65c0ab72c0ed86d0535ea54b5f1f16
Aoi <ht.so.ik0102@gmail.com>
2021-09-29 21:34:22 +0700

    change the link name in aboutme.tsx

--- src/pages/Aboutme.tsx
+++ src/pages/Aboutme.tsx
@@ -28,7 +28,7 @@ const md = `
 > I learned low-code development such as [Outsystems](https://www.outsystems.com/), [Mendix](https://www.mendix.com/).
 > I joined several projects from Australia and Japan, then processed it with client. 
 
- *In addition, I am fan of Blockchain technology. I have experience to develope some DApps with Solidity Node.js and React.js. If you are intersted, please go [my gallery](https://www.outsystems.com/). I think Blockchain techonology has brought many solutions for the problem that we have not been able to solove it. One of these problem is financial. We have been reviewing every existing financial system again by emerging Bitcoin and we have found some problems. I believe that technology has been changing the social system better and blockchain techonology will be one of them. We know that blockchain has still many problems so I would like to help for that.*  
+ *In addition, I am fan of Blockchain technology. I have experience to develope some DApps with Solidity Node.js and React.js. If you are intersted, please go to [my gallery](/works). I think Blockchain techonology has brought many solutions for the problem that we have not been able to solove it. One of these problem is financial. We have been reviewing every existing financial system again by emerging Bitcoin and we have found some problems. I believe that technology has been changing the social system better and blockchain techonology will be one of them. We know that blockchain has still many problems so I would like to help for that.*  
 
 `;
 
