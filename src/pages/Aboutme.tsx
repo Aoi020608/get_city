a8f2aac359375c06ab6dff32e5979824fd15fb61
Aoi <ht.so.ik0102@gmail.com>
2021-09-29 20:59:20 +0700

    remove unused dependencies in Aboutme.tsx

--- src/pages/Aboutme.tsx
+++ src/pages/Aboutme.tsx
@@ -4,15 +4,9 @@ import Container from "@mui/material/Container";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import { Button, CardActionArea, CardActions } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import Box from "@mui/material/Box";
-// import posts from "../Biography.md";
-import Markdown2 from "markdown-to-jsx";
-
+import { CardActionArea } from "@mui/material";
 import Markdown from "markdown-to-jsx";
-import posts from "../blog-post1.md";
+
 import me from "../assets/img/me3.png";
 
 const theme = createTheme();
@@ -37,43 +31,6 @@ const md = `
  *In addition, I am fan of Blockchain technology. I have experience to develope some DApps with Solidity Node.js and React.js. If you are intersted, please go [my gallery](https://www.outsystems.com/). I think Blockchain techonology has brought many solutions for the problem that we have not been able to solove it. One of these problem is financial. We have been reviewing every existing financial system again by emerging Bitcoin and we have found some problems. I believe that technology has been changing the social system better and blockchain techonology will be one of them. We know that blockchain has still many problems so I would like to help for that.*  
 
 `;
-
-// const options = {
-//   overrides: {
-//     h1: {
-//       component: Typography,
-//       props: {
-//         gutterBottom: true,
-//         variant: "h4",
-//         component: "h1",
-//       },
-//     },
-//     h2: {
-//       component: Typography,
-//       props: { gutterBottom: true, variant: "h6", component: "h2" },
-//     },
-//     h3: {
-//       component: Typography,
-//       props: { gutterBottom: true, variant: "subtitle1" },
-//     },
-//     h4: {
-//       component: Typography,
-//       props: {
-//         gutterBottom: true,
-//         variant: "caption",
-//         paragraph: true,
-//       },
-//     },
-//     p: {
-//       component: Typography,
-//       props: { paragraph: true },
-//     },
-//     a: { component: Link },
-//     li: {
-//       component: MarkdownListItem,
-//     },
-//   },
-// };
 
 function Aboutme() {
   return (
