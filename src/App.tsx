1ccf97b728489a5896b698bbbd18a935d7eb1089
Aoi <ht.so.ik0102@gmail.com>
2021-09-29 21:05:28 +0700

    remove root name in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,7 @@ import MainFeaturedPost from "./components/MainFeatured";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import Works from "./pages/Works";
-import Price from "./pages/Price";
 import Aboutme from "./pages/Aboutme";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import ForGotPassword from "./pages/ForgotPassword";
 
 function App() {
   const location = useLocation();
@@ -30,18 +26,6 @@ function App() {
           </Route>
           <Route path="/contact" exact>
             <Contact />
-          </Route>
-          <Route path="/price" exact>
-            <Price />
-          </Route>
-          <Route path="/signin" exact>
-            <SignIn />
-          </Route>
-          <Route path="/signup" exact>
-            <SignUp />
-          </Route>
-          <Route path="/forgot-password" exact>
-            <ForGotPassword />
           </Route>
         </Switch>
       </main>
