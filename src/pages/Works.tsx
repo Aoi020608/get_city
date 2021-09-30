2803dfef4bdd55f64cb243baeea0879df19dddd1
Aoi <ht.so.ik0102@gmail.com>
2021-09-30 19:40:34 +0700

    remove unused dependency

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useHistory } from "react-router";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -18,7 +17,6 @@ import data from "../worksData.json";
 const theme = createTheme();
 
 export default function Works() {
-  const histry = useHistory();
   return (
     <ThemeProvider theme={theme}>
       <Container sx={{ py: 8 }} maxWidth="lg">
