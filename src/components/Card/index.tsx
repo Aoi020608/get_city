1df727734a3fef39532f423e4f8ed8eea2eb7a04
Aoi <ht.so.ik0102@gmail.com>
2022-07-06 11:23:18 +0700

    Add date

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,27 +3,50 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
 
 interface IBlogCardProps {
   title: string;
   body: string;
+  date: string;
 }
 
-export default function BlogCard({ title, body }: IBlogCardProps) {
+export default function BlogCard({ title, body, date }: IBlogCardProps) {
+  const [newDate, setNewDate] = useState("");
+
+  useEffect(() => {
+    const tempDate = new Date(date);
+    setNewDate(tempDate.toLocaleDateString());
+  }, []);
+
   return (
     <Card
-      sx={{ backgroundColor: "#a2a2a2", color: "#000", margin: "3rem 4rem" }}
+      sx={{
+        backgroundColor: "#a2a2a2",
+        color: "#000",
+        margin: "3rem 4rem",
+      }}
     >
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{
+            width: "40rem",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
           {body}
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
         <Button size="small">READ</Button>
+        <Typography variant="body2">{newDate}</Typography>
       </CardActions>
     </Card>
   );
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -3,9 +3,10 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
-import blogData from "../../blogData.json";
+import blogData from "./blogData.json";
 import { instance } from "../../configs/api";
 import BlogCard from "../../components/Card";
+import { Skeleton } from "@mui/material";
 
 const theme = createTheme();
 
@@ -13,8 +14,7 @@ export default function Blog() {
   const [blogs, setBlogs] = useState<Array<any> | null>([{}]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setLoading(true);
+  const fetchAllPostData = () => {
     instance
       .get("/posts")
       .then((res) => {
@@ -27,23 +27,34 @@ export default function Blog() {
         console.error(err);
         setLoading(false);
       });
+  };
 
-    console.log(blogs);
+  useEffect(() => {
+    setLoading(true);
+    setBlogs(blogData);
+    setLoading(false);
   }, []);
 
   return (
     <>
-      {blogs!.length > 0 ? (
+      {!loading && blogs!.length > 0 ? (
         <ThemeProvider theme={theme}>
           <Container sx={{ py: 8 }} maxWidth="lg">
             <div style={{ color: "white" }}>
-              {blogs!.map((blog: any) => (
-                <BlogCard title={blog[0][2]} body={blog[0][3]} />
+              {blogs!.map((blog) => (
+                <BlogCard
+                  title={blog[0][2]}
+                  body={blog[0][3]}
+                  date={blog[0][5]}
+                  key={blog[0]}
+                />
               ))}
             </div>
           </Container>
         </ThemeProvider>
-      ) : null}
+      ) : (
+        <Skeleton animation="wave" />
+      )}
     </>
   );
 }
