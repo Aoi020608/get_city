c32e1249681e64e3158defe6d5f0407324fa0099
Aoi <ht.so.ik0102@gmail.com>
2022-08-08 15:32:28 +0700

    Remove unused file

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 import { BrowserRouter } from "react-router-dom";
 
-import { rootReducer } from "./reducers";
 import App from "./pages/App";
 
 import "antd/dist/antd.css";
 
-const composeEnhancers =
-  (typeof window !== "undefined" &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
